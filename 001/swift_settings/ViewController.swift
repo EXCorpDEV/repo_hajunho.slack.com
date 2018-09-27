8ecf2636771af9f52636e6804a8e80fa082e9d72
hajunho <mynameis@hajunho.com>
2018-09-27 17:17:56 +0900

    Single View App

---
+++ 001/swift_settings/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swift_settings
+//
+//  Created by Junho HA on 2018. 9. 27..
+//  Copyright © 2018년 hajunho.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ 001/swift_settingsTests/swift_settingsTests.swift
@@ -0,0 +1,34 @@
+//
+//  swift_settingsTests.swift
+//  swift_settingsTests
+//
+//  Created by Junho HA on 2018. 9. 27..
+//  Copyright © 2018년 hajunho.com. All rights reserved.
+//
+
+import XCTest
+@testable import swift_settings
+
+class swift_settingsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
