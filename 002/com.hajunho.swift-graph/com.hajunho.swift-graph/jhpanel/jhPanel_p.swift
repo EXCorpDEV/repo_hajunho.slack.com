da71bf16dbb97dd038c36bcb8890b8aad37d079f
hajunho <mynameis@hajunho.com>
2018-10-12 19:18:36 +0900

    [002] definition of protocols that is a blueprint of jhChart

---
+++ 002/com.hajunho.swift-graph/com.hajunho.swift-graph/jhpanel/jhPanel_p.swift
@@ -0,0 +1,13 @@
+//
+//  jhPanel_p.swift
+//  com.hajunho.swift-graph
+//
+//  Created by Junho HA on 2018. 10. 12..
+//  Copyright © 2018년 hajunho.com. All rights reserved.
+//
+
+protocol jhPanel_p {
+    var jhPanelID : Int { get }
+    func initDatas()
+    func drawPanel()
+}
---
+++ 002/com.hajunho.swift-graph/com.hajunho.swift-graph/jhscene/jhScene_p.swift
@@ -0,0 +1,17 @@
+//
+//  jhScene_p.swift
+//  com.hajunho.swift-graph
+//
+//  Created by Junho HA on 2018. 10. 12..
+//  Copyright © 2018년 hajunho.com. All rights reserved.
+//
+
+import UIKit
+
+protocol jhScene_p {
+    func createPanels()
+    func drawBackboard()
+    func drawBackboardLabels()
+    func drawGraph()
+    func drawScene()
+}
