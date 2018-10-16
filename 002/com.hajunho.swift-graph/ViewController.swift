200d3a19cf5e84f88184e3a8b9d7766c72209021
hajunho <mynameis@hajunho.com>
2018-10-16 09:00:37 +0900

    [002] storyboard name is changed.

--- 002/com.hajunho.swift-graph/ViewController.swift
+++ 002/com.hajunho.swift-graph/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  com.hajunho.swift-graph
 //
-//  Created by Junho HA on 2018. 10. 8..
+//  Created by Junho HA on 2018. 10. 16..
 //  Copyright © 2018년 hajunho.com. All rights reserved.
 //
 
@@ -12,9 +12,19 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
