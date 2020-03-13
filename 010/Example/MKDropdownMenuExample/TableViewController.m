efbc910df4353eb5d5de12d2ce55eac34446e97b
Eddie HA <mynameis@hajunho.com>
2020-03-13 18:42:23 +0900

    [010] DropDown Menu for objective-c

---
+++ 010/Example/MKDropdownMenuExample/TableViewController.m
@@ -0,0 +1,79 @@
+//
+//  TableViewController.m
+//  MKDropdownMenuExample
+//
+//  Created by Max Konovalov on 01/04/16.
+//  Copyright © 2016 Max Konovalov. All rights reserved.
+//
+
+#import "TableViewController.h"
+#import "MenuTableViewCell.h"
+
+@interface TableViewController () <MKDropdownMenuDataSource, MKDropdownMenuDelegate>
+@end
+
+@implementation TableViewController
+
+#pragma mark - UITableView
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 15;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    MenuTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"MenuCell" forIndexPath:indexPath];
+    cell.dropdownMenu.dataSource = self;
+    cell.dropdownMenu.delegate = self;
+    cell.dropdownMenu.presentingView = tableView;
+    cell.dropdownMenu.adjustsContentOffset = YES;
+    cell.dropdownMenu.backgroundDimmingOpacity = 0.05;
+    cell.dropdownMenu.dropdownShowsContentAbove = indexPath.row % 2;
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath {
+    MenuTableViewCell *menuCell = (MenuTableViewCell *)cell;
+    [menuCell.dropdownMenu reloadAllComponents];
+}
+
+- (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView {
+    for (MenuTableViewCell *cell in self.tableView.visibleCells) {
+        [cell.dropdownMenu closeAllComponentsAnimated:YES];
+    }
+}
+
+#pragma mark - MKDropdownMenuDataSource
+
+- (NSInteger)numberOfComponentsInDropdownMenu:(MKDropdownMenu *)dropdownMenu {
+    return 3;
+}
+
+- (NSInteger)dropdownMenu:(MKDropdownMenu *)dropdownMenu numberOfRowsInComponent:(NSInteger)component {
+    return 5;
+}
+
+#pragma mark - MKDropdownMenuDelegate
+
+- (NSString *)dropdownMenu:(MKDropdownMenu *)dropdownMenu titleForComponent:(NSInteger)component {
+    NSIndexPath *indexPath = [self.tableView indexPathForRowAtPoint:
+                              [self.tableView convertPoint:CGPointMake(CGRectGetMidX(dropdownMenu.bounds),
+                                                                       CGRectGetMidY(dropdownMenu.bounds))
+                                                  fromView:dropdownMenu]];
+    
+    return [NSString stringWithFormat:@"%zd-%zd", indexPath.row + 1, component + 1];
+}
+
+- (NSString *)dropdownMenu:(MKDropdownMenu *)dropdownMenu titleForRow:(NSInteger)row forComponent:(NSInteger)component {
+    NSIndexPath *indexPath = [self.tableView indexPathForRowAtPoint:
+                              [self.tableView convertPoint:CGPointMake(CGRectGetMidX(dropdownMenu.bounds),
+                                                                       CGRectGetMidY(dropdownMenu.bounds))
+                                                  fromView:dropdownMenu]];
+    
+    return [NSString stringWithFormat:@"%zd-%zd-%zd", indexPath.row + 1, component + 1, row + 1];
+}
+
+- (BOOL)dropdownMenu:(MKDropdownMenu *)dropdownMenu shouldUseFullRowWidthForComponent:(NSInteger)component {
+    return NO;
+}
+
+@end
---
+++ 010/MKDropdownMenu.podspec
@@ -0,0 +1,19 @@
+Pod::Spec.new do |s|
+  s.name             = "MKDropdownMenu"
+  s.version          = "1.4"
+  s.summary          = "Dropdown Menu for iOS"
+  s.description      = <<-DESC
+Dropdown Menu for iOS with many customizable parameters to suit any needs.
+                       DESC
+
+  s.homepage         = "https://github.com/maxkonovalov/MKDropdownMenu"
+  s.screenshots      = "https://raw.github.com/maxkonovalov/MKDropdownMenu/master/Screenshots/MKDropdownMenu.png"
+  s.license          = 'MIT'
+  s.author           = "Max Konovalov"
+  s.source           = { :git => "https://github.com/maxkonovalov/MKDropdownMenu.git", :tag => s.version.to_s }
+
+  s.platform     = :ios, '8.0'
+  s.requires_arc = true
+
+  s.source_files = 'MKDropdownMenu/*'
+end
