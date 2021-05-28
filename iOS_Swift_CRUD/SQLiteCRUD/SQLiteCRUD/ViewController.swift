cb634d8e49aaeecd7fc68692b0fd6bfb64f88e55
Eddie HA <mynameis@hajunho.com>
2021-05-28 14:17:33 +0900

    ViewController Presenting by the code.

--- iOS_Swift_CRUD/SQLiteCRUD/SQLiteCRUD/ViewController.swift
+++ iOS_Swift_CRUD/SQLiteCRUD/SQLiteCRUD/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func onClickButton(_ sender: Any) {
+        debugPrint("onClickButton")
+        let acroView = self.storyboard?.instantiateViewController(withIdentifier: "acroxib") as! ViewControllerACRO
+        acroView.modalPresentationStyle = .fullScreen
+//        acroView.modalPresentationStyle = .currentContext
+//        acroView.modalPresentationStyle = .none //ERROR
+//        acroView.modalPresentationStyle = .overCurrentContext
+//        acroView.modalPresentationStyle = .overFullScreen
+        
+//        acroView.modalPresentationStyle = .automatic
+//        acroView.modalPresentationStyle = .popover
+//        acroView.modalPresentationStyle = .pageSheet
+        
+        self.present(acroView, animated: true, completion: nil)
+    }
 }
 
