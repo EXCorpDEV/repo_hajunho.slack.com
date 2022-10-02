f91448fc6901a3b810bad53023e99e7d8fee2d3a
hajunho <mynameis@hajunho.com>
2022-10-02 13:01:12 +0900

    yarn add babel-plugin-transform-object-rest-spread

---
+++ WEB/react/react-first/dist/main.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (function() { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (function() {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nError: .presets[0][1] must be an object, false, or undefined\\n    at validate (/Users/junhoha/github/repo_hajunho.slack.com/WEB/react/react-first/node_modules/@babel/core/lib/config/validation/options.js:92:25)\\n    at /Users/junhoha/github/repo_hajunho.slack.com/WEB/react/react-first/node_modules/@babel/core/lib/config/config-chain.js:213:34\\n    at cachedFunction (/Users/junhoha/github/repo_hajunho.slack.com/WEB/react/react-first/node_modules/@babel/core/lib/config/caching.js:60:27)\\n    at cachedFunction.next (<anonymous>)\\n    at evaluateSync (/Users/junhoha/github/repo_hajunho.slack.com/WEB/react/react-first/node_modules/gensync/index.js:251:28)\\n    at sync (/Users/junhoha/github/repo_hajunho.slack.com/WEB/react/react-first/node_modules/gensync/index.js:89:14)\\n    at buildRootChain (/Users/junhoha/github/repo_hajunho.slack.com/WEB/react/react-first/node_modules/@babel/core/lib/config/config-chain.js:137:31)\\n    at buildRootChain.next (<anonymous>)\\n    at loadPrivatePartialConfig (/Users/junhoha/github/repo_hajunho.slack.com/WEB/react/react-first/node_modules/@babel/core/lib/config/partial.js:103:62)\\n    at loadPrivatePartialConfig.next (<anonymous>)\");\n\n//# sourceURL=webpack://my-webpack-project/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
