c985faf81749774bf3074b3b430de8a7848037d2
hajunho <mynameis@hajunho.com>
2022-10-02 19:16:03 +0900

    npm i -D html-webpack-plugin

--- WEB/react/react-second/webpack.config.js
+++ WEB/react/react-second/webpack.config.js
@@ -1,30 +1,35 @@
 // webpack.config.js
 module.exports = {
-  mode: 'development', // 1
-  entry: './src/index.js', // 2
-  output: { // 3
-    filename: 'bundle.[hash].js' // 4
-  },
-  module: {
-    rules: [
-      { // 1
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-      { // 2
-        test: /\.html$/,
-        use: [
-          {
-            loader: 'html-loader',
-            options: {
-              minimize: true,
+    mode: 'development', // 1
+    entry: './src/index.js', // 2
+    output: { // 3
+        filename: 'bundle.[hash].js' // 4
+    },
+    module: {
+        rules: [
+            { // 1
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
             },
-          },
+            { // 2
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                        options: {
+                            minimize: true,
+                        },
+                    },
+                ],
+            },
         ],
-      },
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'public/index.html',
+        })
     ],
-  },
 };
