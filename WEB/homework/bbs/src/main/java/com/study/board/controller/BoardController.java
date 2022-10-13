146cb52126518f50cb3f6f0edde05a81ce793bed
sajieun <sajieun000721@naver.com>
2022-10-13 22:55:05 +0900

    DB에 있는것들을 /board/list site에 연결 완료

--- WEB/homework/bbs/src/main/java/com/study/board/controller/BoardController.java
+++ WEB/homework/bbs/src/main/java/com/study/board/controller/BoardController.java
@@ -4,6 +4,7 @@ import com.study.board.entity.Board;
 import com.study.board.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -21,5 +22,11 @@ public class BoardController {
     public String boardWritePro(Board board){
         boardService.write(board);
         return "";
+    }
+
+    @GetMapping("/board/list")
+    public String boardList(Model model){
+        model.addAttribute("list",boardService.boardList());
+        return "boardList";
     }
 }
--- WEB/homework/bbs/src/main/java/com/study/board/service/BoardService.java
+++ WEB/homework/bbs/src/main/java/com/study/board/service/BoardService.java
@@ -5,6 +5,8 @@ import com.study.board.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class BoardService {
     @Autowired //이걸 넣으면 = new BoardRepository이걸 안 해줘도된다.
@@ -12,5 +14,9 @@ public class BoardService {
 
     public void write(Board board){
         boardRepository.save(board);
+    }
+
+    public List<Board> boardList(){
+        return boardRepository.findAll();
     }
 }
