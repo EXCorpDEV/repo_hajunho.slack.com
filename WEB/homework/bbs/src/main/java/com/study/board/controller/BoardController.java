004cb7495725fae5ef399a0483c8e5d2d037632f
sajieun <sajieun000721@naver.com>
2022-10-13 17:40:24 +0900

    entity 생성한 후 contoller연결

--- WEB/homework/bbs/src/main/java/com/study/board/controller/BoardController.java
+++ WEB/homework/bbs/src/main/java/com/study/board/controller/BoardController.java
@@ -1,7 +1,9 @@
 package com.study.board.controller;
 
+import com.study.board.entity.Board;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -10,5 +12,12 @@ public class BoardController {
     @GetMapping("/board/write") //local:8090/board/write
     public String boardWrite(){
         return"boardwrite";
+    }
+    @PostMapping("/board/writepro")
+    public String boardWritePro(Board board){
+
+        System.out.println(board.getTitle());
+
+        return "";
     }
 }
