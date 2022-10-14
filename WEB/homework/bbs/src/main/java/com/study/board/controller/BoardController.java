3494db4b2e28c21cdc607be1deaebfc9633085d7
sajieun <sajieun000721@naver.com>
2022-10-14 09:39:57 +0900

    글 삭제 기능 추가

--- WEB/homework/bbs/src/main/java/com/study/board/controller/BoardController.java
+++ WEB/homework/bbs/src/main/java/com/study/board/controller/BoardController.java
@@ -34,4 +34,10 @@ public class BoardController {
         model.addAttribute("board",boardService.boardView(id));
         return "boardview";
     }
+
+    @GetMapping("/board/delete")
+    public String boardDelete(Integer id){
+        boardService.boardDelete(id);
+        return "redirect:/board/list";
+    }
 }
--- WEB/homework/bbs/src/main/java/com/study/board/service/BoardService.java
+++ WEB/homework/bbs/src/main/java/com/study/board/service/BoardService.java
@@ -23,4 +23,6 @@ public class BoardService {
     public Board boardView(Integer id){
         return boardRepository.findById(id).get();
     }
+    //특정 게시글 삭제
+    public void boardDelete(Integer id){boardRepository.deleteById(id);}
 }
