1f1c7064793562ebc5d04d8f588e4ecbf65bc609
sajieun <sajieun000721@naver.com>
2022-10-26 17:25:53 +0900

    설명 추가

--- WEB/homework/bbs/src/main/java/com/study/board/repository/BoardRepository.java
+++ WEB/homework/bbs/src/main/java/com/study/board/repository/BoardRepository.java
@@ -8,5 +8,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository                                           //엔티티, 기본키 타입?
 public interface BoardRepository extends JpaRepository<Board,Integer> {
+//   find(컬럼이름)Containing -> 컬럼이름을 정할 때 쓴것처럼 단어에 따라 앞은 대문자로 해줘야한다.
     Page<Board> findByTitleContaining(String searchKeyword, Pageable pageable);
 }
