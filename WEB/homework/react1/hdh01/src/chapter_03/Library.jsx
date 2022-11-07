be22a780d84bfc54b87fba06836e83ad0f4457ab
oscar24133 <114084639+oscar24133@users.noreply.github.com>
2022-11-07 20:35:01 +0900

    change

--- WEB/homework/react1/hdh01/src/chapter_03/Library.jsx
+++ WEB/homework/react1/hdh01/src/chapter_03/Library.jsx
@@ -7,6 +7,7 @@ function Library(props) {
       <Book name="처음 만난 파이썬" numOPage={300} />
       <Book name="처음 만난 AWS" numOPage={400} />
       <Book name="처음 만난 리액트" numOPage={500} />
+      
     </div>
   );
 }
