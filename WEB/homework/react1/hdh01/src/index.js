d45b1877e76a6d4e3189d382f35afb6981239ccc
oscar24133 <114084639+oscar24133@users.noreply.github.com>
2022-11-10 17:40:06 +0900

    State & Lifecycle

--- WEB/homework/react1/hdh01/src/index.js
+++ WEB/homework/react1/hdh01/src/index.js
@@ -3,16 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
 import Library from './chapter_03/Library';
 import Clock from './chapter_04/Clock';
 import CommentList from './chapter_05/CommentList';
+import NotificationList from './chapter_06/NotificationList';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CommentList />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <NotificationList />
+  </React.StrictMode>
 );
 
 
