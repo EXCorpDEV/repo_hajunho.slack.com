8fbb4b8b27c78501216a50883f794e9494511c75
oscar24133 <114084639+oscar24133@users.noreply.github.com>
2022-11-14 18:47:30 +0900

    react

--- WEB/homework/react1/hdh01/src/index.js
+++ WEB/homework/react1/hdh01/src/index.js
@@ -4,11 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Library from './chapter_03/Library';
+import Clock from './chapter_04/Clock';
+import CommentList from './chapter_05/CommentList';
+import NotificationList  from './chapter_06/NotificationList';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Library />
+    <NotificationList />
   </React.StrictMode>
 );
 
