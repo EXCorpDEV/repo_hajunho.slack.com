516c11c3713a80714ae8f5fcd594eaf177043c6f
oscar24133 <114084639+oscar24133@users.noreply.github.com>
2022-11-14 16:25:42 +0900

    Hook useState-2

--- WEB/homework/react1/Hook/my-hook/src/App.js
+++ WEB/homework/react1/Hook/my-hook/src/App.js
@@ -1,24 +1,35 @@
 import {useState} from 'react';
 
-function App() {
-  const [time, setTime] = useState(1);
+const heavyWork = () => {
+  console.log("엄청 무거운 작업!!!");
+  return ["한덕현", "김민수"]
+}
 
-  const handleClick = () => {
-    let newTime;
-    if (time >= 12) {
-      newTime = 1;
-    } else {
-      newTime = time + 1;
-    }
-    setTime(newTime);
+function App () {
+  const [names, setNames] = useState(() => {
+    return heavyWork();
+  });
+  const [input, setInput] = useState('');
+
+  const handleInputChange = (e) => {
+    setInput(e.target.value);
   };
 
-  console.log('업데이트!!!');
+const handleUpload = () => {
+  setNames((prevState) => {
+    console.log('이전 state: ', prevState);
+    return [input, ...prevState];
+  });
+};
+
 
   return (
     <div>
-      <span>현재 시각: {time}시</span>
-      <button onClick={handleClick}>Update</button>
+      <input type="text" value={input} onChange={handleInputChange}/>
+      <button onClick={handleUpload}>Upload</button>
+      {names.map((name, idx) => {
+        return <p key={idx}>{name}</p>;
+      })}
     </div>
   );
 }
