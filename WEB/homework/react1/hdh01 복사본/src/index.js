4b837a2942873ec780f87dd62d8c09cbf622417e
oscar24133 <114084639+oscar24133@users.noreply.github.com>
2022-11-15 16:06:29 +0900

    hook all

--- WEB/homework/react1/hdh01 복사본/src/index.js
+++ WEB/homework/react1/hdh01 복사본/src/index.js
@@ -7,11 +7,12 @@ import Library from './chapter_03/Library';
 import Clock from './chapter_04/Clock';
 import CommentList from './chapter_05/CommentList';
 import NotificationList  from './chapter_06/NotificationList';
+import Accommodate from './chapter_07/Accommodate';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Accommodate />
   </React.StrictMode>
 );
 
--- WEB/homework/react1/hdh01 복사본/src/useEffect.js
+++ WEB/homework/react1/hdh01 복사본/src/useEffect.js
@@ -11,7 +11,7 @@ function Counter(props) {
     <div>
       <p>총 {count}번 클릭했습니다.</p>
       <button onClick={() => setCount(count + 1)}>
-        클릭
+        클릭+-
       </button>
     </div>
   )
