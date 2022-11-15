17e96fa124b26c333906e452e0f7a755a322a947
Joe <mynameis@hajunho.com>
2022-11-15 22:15:08 +0900

    react basic codes + node modules freezing

---
+++ WEB/react/react-fifth/src/ShopLst.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ShopLst = (
+        <>
+          <h1>Shopping List for {this.props.name}</h1>
+          <ul>
+            <li>Instagram</li>
+            <li>WhatsApp</li>
+            <li>Oculus</li>
+          </ul>
+        </>
+);
+
+export default ShopLst;
+export {ShopLst};
