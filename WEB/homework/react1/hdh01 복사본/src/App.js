b99ad0247786841d45bbf3717fdc72366d00ea7f
oscar24133 <114084639+oscar24133@users.noreply.github.com>
2022-11-15 11:29:13 +0900

    react

---
+++ WEB/homework/react1/hdh01 복사본/src/App.js
@@ -0,0 +1,36 @@
+import {useState} from 'react';
+
+const heavyWork = () => {
+  console.log("엄청 무거운 작업!!!");
+  return ["한덕현", "김민수"]
+}
+
+function App () {
+  const [names, setNames] = useState(() => {
+    return heavyWork();
+  });
+  const [input, setInput] = useState('');
+
+  const handleInputChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleUpload = () => {
+    setNames((prevState) => {
+      console.log('이전 state: ', prevState);
+      return [input, ...prevState];
+    });
+  };
+  
+  return (
+  <div>
+    <input type="text" value={input} onChange={handleInputChange}/>
+      <button onClick={handleUpload}>Upload</button>
+      {names.map((name, idx) => {
+      return <p key={idx}>{name}</p>;
+    })}
+</div>
+);
+}
+
+export default App;
