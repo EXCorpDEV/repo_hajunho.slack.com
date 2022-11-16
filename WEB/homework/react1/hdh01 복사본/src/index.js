c156eb04b46e59b3efcf807f57c6d0067fd98ac3
oscar24133 <114084639+oscar24133@users.noreply.github.com>
2022-11-16 10:10:58 +0900

    Eventhandler ConfirmButton

--- WEB/homework/react1/hdh01 복사본/src/index.js
+++ WEB/homework/react1/hdh01 복사본/src/index.js
@@ -8,11 +8,12 @@ import Clock from './chapter_04/Clock';
 import CommentList from './chapter_05/CommentList';
 import NotificationList  from './chapter_06/NotificationList';
 import Accommodate from './chapter_07/Accommodate';
+import ConfirmButton from './chapter_08/ConfirmButton';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Accommodate />
+    <ConfirmButton />
   </React.StrictMode>
 );
 
