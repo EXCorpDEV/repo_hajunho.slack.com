70934ddaf5380fde6bf26296a8f231b01e01e0c5
oscar24133 <114084639+oscar24133@users.noreply.github.com>
2022-11-18 09:43:48 +0900

    form

--- WEB/homework/react1/hdh01 복사본/src/index.js
+++ WEB/homework/react1/hdh01 복사본/src/index.js
@@ -11,11 +11,12 @@ import Accommodate from './chapter_07/Accommodate';
 import ConfirmButton from './chapter_08/ConfirmButton';
 import LandingPage from './chapter_09/LandingPage';
 import AttendanceBook from './chapter_10/AttendanceBook';
+import SignUp from './chapter_11/SignUp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AttendanceBook />
+    <SignUp />
   </React.StrictMode>
 );
 
