e31e52666ce14871b32b7239476fcdeb8e487042
oscar24133 <114084639+oscar24133@users.noreply.github.com>
2022-11-20 17:53:52 +0900

    styled-components

--- WEB/homework/react1/hdh01 복사본/src/index.js
+++ WEB/homework/react1/hdh01 복사본/src/index.js
@@ -15,11 +15,12 @@ import SignUp from './chapter_11/SignUp';
 import Calculator from './chapter_12/Calculator';
 import ProfileCard from './chapter_13/ProfileCard';
 import DarkOrLight from './chapter_14/DarkOrLight';
+import Blocks from './chapter_15/Blocks';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <DarkOrLight />
+    <Blocks />
   </React.StrictMode>
 );
 
