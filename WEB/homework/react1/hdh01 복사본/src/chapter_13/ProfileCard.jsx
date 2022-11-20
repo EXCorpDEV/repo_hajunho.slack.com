7381b12c1aa28c0cb5fba7d72d228b5c7b0f0598
oscar24133 <114084639+oscar24133@users.noreply.github.com>
2022-11-20 15:40:47 +0900

    Containment & Specialization

---
+++ WEB/homework/react1/hdh01 복사본/src/chapter_13/ProfileCard.jsx
@@ -0,0 +1,12 @@
+import Card from "./Card"
+
+function profileCard(props){
+  return(
+    <Card title="deockHyun HAN" backgroundColor="#4ea04e">
+      <p>안녕하세요 쏘닥입니다.</p>
+      <p>쏘닥은 리액트를 사용해 개발하고 있습니다.</p>
+    </Card>
+  );
+}
+
+export default profileCard;
--- WEB/homework/react1/hdh01 복사본/src/index.js
+++ WEB/homework/react1/hdh01 복사본/src/index.js
@@ -13,11 +13,12 @@ import LandingPage from './chapter_09/LandingPage';
 import AttendanceBook from './chapter_10/AttendanceBook';
 import SignUp from './chapter_11/SignUp';
 import Calculator from './chapter_12/Calculator';
+import ProfileCard from './chapter_13/ProfileCard';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Calculator />
+    <ProfileCard />
   </React.StrictMode>
 );
 
