2702f6020d96fc5fb385bf77f0c05f6fb5da133f
hajunho <mynameis@hajunho.com>
2024-03-24 21:53:34 +0900

    Go with "from deskew import determine_skew"

--- ubuntu/python310/skew/hjhskew.py
+++ ubuntu/python310/skew/hjhskew.py
@@ -5,23 +5,11 @@ from skimage import io
 from skimage.feature import canny
 from skimage.transform import hough_line, hough_line_peaks
 from skimage.color import rgb2gray
+from deskew import determine_skew
 from io import BytesIO
 
-def determine_skew(image):
-    if image.ndim == 3:
-        image = rgb2gray(image)
-    edges = canny(image, sigma=3.0)
-    h, theta, d = hough_line(edges)
-    angles = [np.rad2deg(angle) for _, angle, _ in zip(*hough_line_peaks(h, theta, d))]
-    if not angles:
-        return 0
-    median_angle = np.median(angles)
-    return median_angle
-
 def deskew_and_save_image(image_path, output_path):
-    with open(image_path, 'rb') as f:
-        image_bytes = f.read()
-    image = Image.open(BytesIO(image_bytes)).convert('RGB')
+    image = Image.open(image_path).convert('RGB')
     image_array = np.array(image)
 
     angle = determine_skew(image_array)
