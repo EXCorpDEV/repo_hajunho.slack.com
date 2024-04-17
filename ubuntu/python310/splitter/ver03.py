3dccbe6d533850fe83ff72983d2caa428b8e94ca
hajunho <mynameis@hajunho.com>
2024-04-17 14:07:13 +0900

    from scipy import ndimage

--- ubuntu/python310/splitter/ver03.py
+++ ubuntu/python310/splitter/ver03.py
@@ -4,6 +4,7 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
 from tensorflow.keras.optimizers import Adam
+from scipy import ndimage
 
 os.environ['TF_XLA_FLAGS'] = '--tf_xla_auto_jit=0'
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
