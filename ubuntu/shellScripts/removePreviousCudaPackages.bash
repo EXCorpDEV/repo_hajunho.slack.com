b1e5db192b0bccda12ef8daa160f80e7bfb96162
hajunho <mynameis@hajunho.com>
2024-04-26 10:35:02 +0900

    When the command "apt --fix-broken install "  is not working

---
+++ ubuntu/shellScripts/removePreviousCudaPackages.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# dpkg 명령어를 사용하여 "cuda"와 관련된 패키지 목록을 가져옵니다.
+packages=$(dpkg -l | grep -i cuda | awk '{print $2}')
+
+# 패키지 목록을 반복하면서 각 패키지를 삭제합니다.
+for package in $packages; do
+    sudo dpkg --purge --force-all "$package"
+done
+
+echo "CUDA 관련 패키지 삭제가 완료되었습니다."
