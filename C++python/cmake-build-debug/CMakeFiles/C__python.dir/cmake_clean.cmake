78cdcfa825cf510589dcd8025e9c478c04306997
hajunho <mynameis@hajunho.com>
2025-04-14 10:49:32 +0900

    can't follow the works in one's computer

---
+++ C++python/cmake-build-debug/CMakeFiles/C__python.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C__python.dir/Linspace.cpp.o"
+  "CMakeFiles/C__python.dir/Linspace.cpp.o.d"
+  "C__python"
+  "C__python.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/C__python.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
