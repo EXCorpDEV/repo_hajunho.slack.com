841b4a8237e1ec5417f7e80b6cc85d17094e3204
hajunho <mynameis@hajunho.com>
2025-05-20 15:55:13 +0900

    intelli J setting

--- iOS/Abandoned/kotlin_examples/ktsqlexam01/src/Main.kt
+++ iOS/Abandoned/kotlin_examples/ktsqlexam01/src/Main.kt
@@ -1,22 +1,24 @@
-//fun main() {
-//    println("✅ Hello from Kotlin")
-//}
-
 import java.sql.Connection
 import java.sql.DriverManager
 
-fun main() {
-    Class.forName("org.sqlite.JDBC") // 꼭 필요함
-    val conn: Connection = DriverManager.getConnection("jdbc:sqlite:sample.db")
+class App {
 
-    val stmt = conn.createStatement()
-    stmt.executeUpdate("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT)")
-    stmt.executeUpdate("INSERT INTO users (name) VALUES ('홍길동')")
-    val rs = stmt.executeQuery("SELECT * FROM users")
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            Class.forName("org.sqlite.JDBC") // 꼭 필요함
+            val conn: Connection = DriverManager.getConnection("jdbc:sqlite:sample.db")
 
-    while (rs.next()) {
-        println("👤 id=${rs.getInt("id")}, name=${rs.getString("name")}")
+            val stmt = conn.createStatement()
+            stmt.executeUpdate("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT)")
+            stmt.executeUpdate("INSERT INTO users (name) VALUES ('홍길동')")
+            val rs = stmt.executeQuery("SELECT * FROM users")
+
+            while (rs.next()) {
+                println("👤 id=${rs.getInt("id")}, name=${rs.getString("name")}")
+            }
+
+            conn.close()
+        }
     }
-
-    conn.close()
 }
